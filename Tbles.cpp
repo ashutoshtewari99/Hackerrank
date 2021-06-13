267b2362e9fc430250a058d8c688eaff81c8ed4b
ASHUTOSH TEWARI <54748312+ashutoshtewari99@users.noreply.github.com>
2021-06-13 20:39:58 +0530

    commit

---
+++ Tbles.cpp
@@ -0,0 +1,18 @@
+#include "/Users/mac/stdc++.h"
+using namespace std;
+int main()
+{
+cout<<"\n-------------------------------------\n";
+int n;
+scanf("%d", &n);
+for(int i=1;i<=n;i++)
+{
+  for(int j=1;j<=n;j++)
+  {
+    printf("%d ", i*j);
+  }
+  printf("\n");
+}
+cout<<"\n-------------------------------------\n";
+ return 0;
+}
